3ef2fad2b42853018f86074cf2b2a302d5ce24fb
Lilia Sviridova <svirivoda@gmail.com>
2024-06-14 12:45:58 +0300

    Refactor code after review

--- components/CompletionScoreCard.tsx
+++ components/CompletionScoreCard.tsx
@@ -2,13 +2,20 @@
 
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import Chip from '@mui/material/Chip';
+import {
+  Box,
+  Button,
+  Typography,
+  Link,
+  Chip,
+  useTheme,
+  Card,
+  CardContent,
+  Stack
+} from '@mui/material';
+import { ArrowUpRight, CircleFill } from 'react-bootstrap-icons';
+
+
 
 interface CompletionScoreCardProps {}
 
@@ -16,11 +23,11 @@ interface MockData {
   title: string;
   linkText: string;
   linkHref: string;
-  scoreData: scoreData[];
+  scoreData: ScoreData[];
 }
 
-interface scoreData {
-  status: 'High' | 'Medium' | 'Low';
+interface ScoreData {
+  status: 'High' | 'Moderate' | 'Low';
   total: number;
   completed: number;
 }
@@ -30,34 +37,45 @@ const mockData = {
   linkText: 'netzerocities.kausal.tech/coventry-netzero',
   linkHref: '#',
   scoreData: [
-    { status: 'High', total: 13, completed: 10 } as scoreData,
-    { status: 'Medium', total: 10, completed: 7 } as scoreData,
-    { status: 'Low', total: 8, completed: 2 } as scoreData,
+    { status: 'High', total: 13, completed: 6 } as ScoreData,
+    { status: 'Moderate', total: 10, completed: 5 } as ScoreData,
+    { status: 'Low', total: 8, completed: 3 } as ScoreData,
   ],
 };
 
-const calculateScores = (scoreData: scoreData[]) => {
+
+
+const calculateScores = (scoreData: ScoreData[]) => {
   return scoreData.map((data) => ({
     status: data.status,
     score: `${data.completed}/${data.total}`,
   }));
 };
 
-const calculatePercentage = (scoreData: scoreData[]) => {
-  const totalCompleted = scoreData.reduce(
-    (acc, curr) => acc + curr.completed,
-    0
-  );
-  const totalItems = scoreData.reduce((acc, curr) => acc + curr.total, 0);
-  return totalItems === 0 ? 0 : (totalCompleted / totalItems) * 100;
+const calculatePercentage = (scoreData: ScoreData[]) => {
+  const coeff: { [key: string]: number } = {
+    High: 0.6,
+    Moderate: 0.3,
+    Low: 0.1,
+  };
+  const totalPercentage = scoreData.reduce((acc, data) => {
+    const completionPercentage =
+      data.total > 0 ? data.completed / data.total : 0;
+    return acc + completionPercentage * (coeff[data.status] || 0);
+  }, 0);
+
+  return totalPercentage * 100;
 };
 
 export const CompletionScoreCard: React.FC<CompletionScoreCardProps> = () => {
   const [data, setData] = useState<MockData | null>(null);
   const [scores, setScores] = useState<
-    { status: 'High' | 'Medium' | 'Low'; score: string }[]
+    { status: 'High' | 'Moderate' | 'Low'; score: string }[]
   >([]);
+
   const [completionPercentage, setCompletionPercentage] = useState<number>(0);
+
+  const theme = useTheme();
 
   useEffect(() => {
     setTimeout(() => {
@@ -73,140 +91,136 @@ if (!data) {
   return <Typography>Loading...</Typography>;
 }
 
+ const getStatusColor = (status: string) => {
+   switch (status) {
+     case 'High':
+       return theme.palette.error.main;
+     case 'Moderate':
+       return theme.palette.warning.main;
+     case 'Low':
+       return theme.palette.success.main;
+   }
+ };
+
   return (
-    <Box
-      sx={{
-        width: '80%',
-        margin: '0 auto',
-        marginTop: '7em',
-        border: '1px solid #ddd',
-        borderRadius: '0.5em',
-        padding: '1em',
-        backgroundColor: '#f9f9f9',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      }}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          marginBottom: '1em',
-        }}
-      >
-        <Box>
-          <Typography variant="h5">{data.title}</Typography>
-          <Box
-            component="span"
-            sx={{
-              display: 'inline-flex',
-              alignItems: 'center',
-              textDecoration: 'none',
-              '&:hover': {
-                borderBottom: '1px solid',
-              },
-            }}
-          >
-            <Link
-              href={data.linkHref}
-              variant="body2"
+    <Card>
+      <CardContent>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={2}
+          sx={{ marginBottom: 2 }}
+        >
+          <Box>
+            <Typography variant="h5">{data.title}</Typography>
+            <Box
+              component="span"
               sx={{
                 display: 'inline-flex',
                 alignItems: 'center',
                 textDecoration: 'none',
-              }}
-            >
-              {data.linkText}
-              <ArrowUpwardIcon
-                sx={{
-                  fontSize: '1.1em',
-                  transform: 'rotate(45deg)',
-                  marginLeft: '0.3em',
-                }}
-              />
-            </Link>
-          </Box>
-        </Box>
-        <Button
-          sx={{
-            backgroundColor: 'transparent',
-            color: 'primary.main',
-            '&:hover': {
-              backgroundColor: 'transparent',
-              color: 'primary.main',
-              border: '1px solid',
-              borderColor: 'brand.800',
-            },
-          }}
-          variant="contained"
-          color="primary"
-          href="/dashboard"
-        >
-          Explore city dashboard{' '}
-          <ArrowUpwardIcon
-            sx={{
-              transform: 'rotate(45deg)',
-              fontSize: '1.2em',
-              marginLeft: '0.5em',
-            }}
-          />
-        </Button>
-      </Box>
-      <Box
-        sx={{
-          width: '60%',
-          backgroundColor: '#f0f0f0',
-          padding: '1em',
-          borderRadius: '0.5em',
-        }}
-      >
-        <Box
-          sx={{ display: 'flex', alignItems: 'center', marginBottom: '0.5em' }}
-        >
-          <FiberManualRecordIcon sx={{ color: 'red', marginRight: '0.5em' }} />
-          <Typography variant="subtitle1">
-            Data completion score: {completionPercentage.toFixed(0)}%
-          </Typography>
-        </Box>
-        <Typography variant="body1" paragraph>
-          Your score assesses the completeness of city data, assigning weights
-          based on the importance of city-specific values for precise
-          projections. Enter values in the Data Collection. Centre below to
-          boost your score and enable more accurate projections.
-        </Typography>
-        <Box sx={{ display: 'flex', gap: '0.5em' }}>
-          {scores.map((score) => (
-            <Chip
-              key={score.status}
-              icon={
-                <FiberManualRecordIcon
-                  sx={{
-                    color:
-                      score.status === 'High'
-                        ? 'red'
-                        : score.status === 'Medium'
-                        ? 'orange'
-                        : 'green',
-                  }}
-                />
-              }
-              label={`${score.status}: ${score.score}`}
-              sx={{
-                '& .MuiChip-icon': {
-                  color: `${
-                    score.status === 'High'
-                      ? 'red'
-                      : score.status === 'Medium'
-                      ? 'orange'
-                      : 'green'
-                  } !important`,
+                '&:hover': {
+                  borderBottom: '1px solid',
                 },
               }}
+            >
+              <Link
+                href={data.linkHref}
+                variant="body2"
+                sx={{
+                  display: 'inline-flex',
+                  alignItems: 'center',
+                  textDecoration: 'none',
+                  color: (theme) => theme.palette.grey[700],
+                }}
+              >
+                {data.linkText}
+                <ArrowUpRight
+                  style={{
+                    marginLeft: '0.5em',
+                  }}
+                />
+              </Link>
+            </Box>
+          </Box>
+          <Button
+            component={Link}
+            sx={{
+              backgroundColor: 'transparent',
+              color: 'inherit',
+              '&:hover': {
+                backgroundColor: 'transparent',
+                color: 'inherit',
+                border: '1px solid',
+                borderColor: 'inherit',
+              },
+            }}
+            variant="contained"
+            color="primary"
+            href="/dashboard"
+          >
+            Explore city dashboard{' '}
+            <ArrowUpRight
+              style={{
+                marginLeft: '0.5em',
+              }}
             />
-          ))}
+          </Button>
+        </Stack>
+        <Box
+          sx={{
+            width: '60%',
+            backgroundColor: 'grey.200',
+            padding: 1,
+            borderRadius: 0.5,
+          }}
+        >
+          <Stack
+            direction="row"
+            alignItems="center"
+            spacing={1}
+            sx={{ marginBottom: 2 }}
+          >
+            <CircleFill
+              style={{
+                color: theme.palette.error.main,
+              }}
+            />
+            <Typography variant="subtitle1">
+              Data completion score: {completionPercentage.toFixed(0)}%
+            </Typography>
+          </Stack>
+          <Typography variant="body1" paragraph marginBottom={2}>
+            Your score assesses the completeness of city data, assigning weights
+            based on the importance of city-specific values for precise
+            projections. Enter values in the Data Collection. Centre below to
+            boost your score and enable more accurate projections.
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            {scores.map((score) => (
+              <Chip
+                key={score.status}
+                icon={
+                  <CircleFill
+                    style={{
+                      color: getStatusColor(score.status),
+                      marginLeft: '0.5em',
+                    }}
+                  />
+                }
+                label={`${score.status}: ${score.score}`}
+                sx={{
+                  '& .MuiChip-icon': {
+                    color: getStatusColor(score.status),
+                  },
+                }}
+              />
+            ))}
+          </Stack>
         </Box>
-      </Box>
-    </Box>
+      </CardContent>
+    </Card>
   );
 };
 
