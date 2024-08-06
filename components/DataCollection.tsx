eca13b1a56e018cff8f0436d59649f5edc4ee5e1
Lilia Sviridova <svirivoda@gmail.com>
2024-08-06 15:06:24 +0300

    Implement conditional rendering of DataCollection blocks

--- components/DataCollection.tsx
+++ components/DataCollection.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import * as React from 'react';
-import { Box, Tabs, Tab, Card, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Tabs, Tab, Card, Typography, Collapse } from '@mui/material';
+import EssentialData from './EssentialData';
 import { DatasheetEditor } from '@/components/DatasheetEditor';
 import { Tab as TTab, useDataCollectionStore } from '@/store/data-collection';
 import { GetMeasureTemplatesQuery } from '@/types/__generated__/graphql';
@@ -38,11 +39,13 @@ function a11yProps(tab: TTab) {
 
 type Props = {
   measureTemplates: NonNullable<GetMeasureTemplatesQuery['framework']>;
+  baselineYear: string;
 };
 
-const DataCollection = ({ measureTemplates }: Props) => {
+const DataCollection = ({ measureTemplates, baselineYear }: Props) => {
   const setSelectedTab = useDataCollectionStore((state) => state.setTab);
   const selectedTab = useDataCollectionStore((state) => state.selectedTab);
+  const [essentialDataCompleted, setEssentialDataCompleted] = useState(false);
 
   const handleChange = (event: React.SyntheticEvent, newSelected: TTab) => {
     setSelectedTab(newSelected);
@@ -56,44 +59,59 @@ const DataCollection = ({ measureTemplates }: Props) => {
     ? mapMeasureTemplatesToRows(measureTemplates.futureAssumptions)
     : undefined;
 
+  const handleEssentialDataChange = (field: string, value: number) => {
+    console.log(`Field ${field} changed to ${value}`);
+  };
+
+  const handleEssentialDataComplete = () => {
+    setEssentialDataCompleted(true);
+  };
+
   return (
     <Card>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs
-          value={selectedTab}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="Data collection" value="data" {...a11yProps('data')} />
-          <Tab
-            label="Future assumptions (2030)"
-            value="assumptions"
-            {...a11yProps('assumptions')}
-          />
-        </Tabs>
-      </Box>
-      <CustomTabPanel selected={selectedTab} index={'data'}>
-        <Typography variant="subtitle2" paragraph gutterBottom>
-          Collect essential data about your city&apos;s current state across key
-          sectors. This phase focuses on gathering raw data to establish a
-          baseline for your city&apos;s climate initiatives.
-        </Typography>
-        {/* TODO: Fallback component */}
-        {!!dataMeasures && (
-          <DatasheetEditor sections={dataMeasures} withIndexes />
-        )}
-      </CustomTabPanel>
-      <CustomTabPanel selected={selectedTab} index={'assumptions'}>
-        <Typography variant="subtitle2" paragraph gutterBottom>
-          These assumptions should reflect an ambitious yet feasible scenario
-          aligned with the city&apos;s Climate Action Plan, indicating the
-          extent to which the city aims to achieve decarbonisation goals.
-        </Typography>
-        {/* TODO: Fallback component */}
-        {!!assumptionMeasures && (
-          <DatasheetEditor sections={assumptionMeasures} />
-        )}
-      </CustomTabPanel>
+      <EssentialData
+        baselineYear={baselineYear}
+        onChange={handleEssentialDataChange}
+        onComplete={handleEssentialDataComplete}
+      />
+      <Collapse in={essentialDataCompleted} timeout="auto" unmountOnExit>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs
+            value={selectedTab}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Data collection" value="data" {...a11yProps('data')} />
+            <Tab
+              label="Future assumptions (2030)"
+              value="assumptions"
+              {...a11yProps('assumptions')}
+            />
+          </Tabs>
+        </Box>
+        <CustomTabPanel selected={selectedTab} index={'data'}>
+          <Typography variant="subtitle2" paragraph gutterBottom>
+            Collect essential data about your city&apos;s current state across
+            key sectors. This phase focuses on gathering raw data to establish a
+            baseline for your city&apos;s climate initiatives.
+          </Typography>
+          {/* TODO: Fallback component */}
+          {!!dataMeasures && (
+            <DatasheetEditor sections={dataMeasures} withIndexes />
+          )}
+        </CustomTabPanel>
+        <CustomTabPanel selected={selectedTab} index={'assumptions'}>
+          <Typography variant="subtitle2" paragraph gutterBottom>
+            These assumptions should reflect an ambitious yet feasible scenario
+            aligned with the city&apos;s Climate Action Plan, indicating the
+            extent to which the city aims to achieve decarbonisation goals.
+          </Typography>
+          {/* TODO: Fallback component */}
+          {!!assumptionMeasures && (
+            <DatasheetEditor sections={assumptionMeasures} />
+          )}
+        </CustomTabPanel>
+      </Collapse>
     </Card>
   );
 };
--- components/EssentialData.tsx
+++ components/EssentialData.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import {
   Typography,
   Box,
-  Card,
   Accordion as MuiAccordion,
   AccordionSummary as MuiAccordionSummary,
   AccordionDetails as MuiAccordionDetails,
@@ -17,22 +16,8 @@ import { NumberFormatValues } from 'react-number-format';
 const Accordion = styled((props: AccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
-  '&:first-of-type': {
-    borderTopLeftRadius: theme.shape.borderRadius,
-    borderTopRightRadius: theme.shape.borderRadius,
-  },
-  '&:last-of-type': {
-    borderBottomLeftRadius: theme.shape.borderRadius,
-    borderBottomRightRadius: theme.shape.borderRadius,
-  },
-  border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
-    borderBottom: 0,
-  },
-  '&::before': {
-    display: 'none',
-  },
-  marginBottom: '10px', // Add bottom margin to the accordion
+  borderTopLeftRadius: theme.shape.borderRadius,
+  borderTopRightRadius: theme.shape.borderRadius,
 }));
 
 const AccordionSummary = styled(MuiAccordionSummary)(({ theme }) => ({
@@ -43,18 +28,19 @@ const AccordionSummary = styled(MuiAccordionSummary)(({ theme }) => ({
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  borderTop: 'none',
 }));
 
-interface EssentialDataPanelProps {
+interface EssentialDataProps {
   baselineYear: string;
   onChange: (field: string, value: number) => void;
+  onComplete: () => void;
 }
 
-const EssentialDataPanel: React.FC<EssentialDataPanelProps> = ({
+const EssentialData: React.FC<EssentialDataProps> = ({
   baselineYear,
   onChange,
+  onComplete,
 }) => {
   const [population, setPopulation] = useState<number | null>(null);
   const [cityGDP, setCityGDP] = useState<number | null>(null);
@@ -64,7 +50,6 @@ const EssentialDataPanel: React.FC<EssentialDataPanelProps> = ({
   const [heatingDemand, setHeatingDemand] = useState<number | null>(null);
   const [isCollapsed, setIsCollapsed] = useState(false);
 
-  // Check if all data is provided
   const allDataProvided =
     population !== null &&
     cityGDP !== null &&
@@ -75,9 +60,10 @@ const EssentialDataPanel: React.FC<EssentialDataPanelProps> = ({
     if (allDataProvided) {
       setTimeout(() => {
         setIsCollapsed(true);
-      }, 300); // Delay to allow the user to see the tick mark
+        onComplete();
+      }, 300);
     }
-  }, [allDataProvided]);
+  }, [allDataProvided, onComplete]);
 
   const handleBlur = (field: string) => (values: NumberFormatValues) => {
     if (!isNaN(values.floatValue)) {
@@ -86,147 +72,125 @@ const EssentialDataPanel: React.FC<EssentialDataPanelProps> = ({
   };
 
   const commonProps = {
-    sx: { m: 1, height: '40px', '.MuiInputBase-root': { height: '40px' } },
-    fullWidth: true,
-    margin: 'dense' as const,
+    sx: { m: 1, height: '30', '.MuiInputBase-root': { height: '30' } },
   };
 
   return (
-    <Card>
-      <Accordion
-        expanded={!isCollapsed}
-        onChange={() => setIsCollapsed(!isCollapsed)}
-      >
-        <AccordionSummary expandIcon={<ChevronDown />}>
-          <Box display="flex" alignItems="center">
-            <Typography variant="h5">
-              Essential Data ({baselineYear})
-            </Typography>
-            {allDataProvided && (
-              <CheckCircle style={{ color: 'green', marginLeft: '10px' }} />
-            )}
-          </Box>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography variant="subtitle2" paragraph gutterBottom>
-            Provide the following key data points to enhance the accuracy of our
-            projections and assumptions:
+    <Accordion
+      expanded={!isCollapsed}
+      onChange={() => setIsCollapsed(!isCollapsed)}
+    >
+      <AccordionSummary expandIcon={<ChevronDown />}>
+        <Box display="flex" alignItems="center">
+          <Typography variant="h6">Essential Data ({baselineYear})</Typography>
+          {allDataProvided && (
+            <CheckCircle style={{ color: 'green', marginLeft: '10px' }} />
+          )}
+        </Box>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography variant="subtitle2" paragraph gutterBottom>
+          Provide the following key data points to enhance the accuracy of our
+          projections and assumptions:
+        </Typography>
+        <Box>
+          <Typography variant="subtitle2" gutterBottom>
+            Economic and demographic indicators
           </Typography>
-          <Box>
-            <Typography variant="subtitle2" gutterBottom>
-              Economic and demographic indicators
-            </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-              <Box>
-                <Typography
-                  variant="body2"
-                  gutterBottom
-                  sx={{ marginBottom: '4px' }}
-                >
-                  Population
-                </Typography>
-                <NumberInput
-                  value={population !== null ? population : ''}
-                  onValueChange={(values) =>
-                    setPopulation(values.floatValue ?? null)
-                  }
-                  onBlur={(e) =>
-                    handleBlur('population')(e as unknown as NumberFormatValues)
-                  }
-                  {...commonProps}
-                  sx={{ width: '25%' }}
-                />
-              </Box>
-              <Box>
-                <Typography
-                  variant="body2"
-                  gutterBottom
-                  sx={{ marginBottom: '4px' }}
-                >
-                  City GDP
-                </Typography>
-                <NumberInput
-                  value={cityGDP !== null ? cityGDP : ''}
-                  onValueChange={(values) =>
-                    setCityGDP(values.floatValue ?? null)
-                  }
-                  onBlur={(e) =>
-                    handleBlur('cityGDP')(e as unknown as NumberFormatValues)
-                  }
-                  {...commonProps}
-                  sx={{ width: '25%' }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">€</InputAdornment>
-                    ),
-                  }}
-                />
-              </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+            <Box>
+              <Typography variant="body2" gutterBottom>
+                Population
+              </Typography>
+              <NumberInput
+                value={population !== null ? population : ''}
+                onValueChange={(values) =>
+                  setPopulation(values.floatValue ?? null)
+                }
+                onBlur={(e) =>
+                  handleBlur('population')(e as unknown as NumberFormatValues)
+                }
+                {...commonProps}
+                sx={{ width: '25%' }}
+              />
             </Box>
-            <Typography variant="subtitle2" gutterBottom mt={2}>
-              Energy consumption metrics
-            </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-              <Box>
-                <Typography
-                  variant="body2"
-                  gutterBottom
-                  sx={{ marginBottom: '4px' }}
-                >
-                  Total Electricity Demand
-                </Typography>
-                <NumberInput
-                  value={electricityDemand !== null ? electricityDemand : ''}
-                  onValueChange={(values) =>
-                    setElectricityDemand(values.floatValue ?? null)
-                  }
-                  onBlur={(e) =>
-                    handleBlur('electricityDemand')(
-                      e as unknown as NumberFormatValues
-                    )
-                  }
-                  {...commonProps}
-                  sx={{ width: '25%' }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">MWh</InputAdornment>
-                    ),
-                  }}
-                />
-              </Box>
-              <Box>
-                <Typography
-                  variant="body2"
-                  gutterBottom
-                  sx={{ marginBottom: '4px' }}
-                >
-                  Total Heating Demand
-                </Typography>
-                <NumberInput
-                  value={heatingDemand !== null ? heatingDemand : ''}
-                  onValueChange={(values) =>
-                    setHeatingDemand(values.floatValue ?? null)
-                  }
-                  onBlur={(e) =>
-                    handleBlur('heatingDemand')(
-                      e as unknown as NumberFormatValues
-                    )
-                  }
-                  {...commonProps}
-                  sx={{ width: '25%' }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">GWh</InputAdornment>
-                    ),
-                  }}
-                />
-              </Box>
+            <Box>
+              <Typography variant="body2" gutterBottom>
+                City GDP
+              </Typography>
+              <NumberInput
+                value={cityGDP !== null ? cityGDP : ''}
+                onValueChange={(values) =>
+                  setCityGDP(values.floatValue ?? null)
+                }
+                onBlur={(e) =>
+                  handleBlur('cityGDP')(e as unknown as NumberFormatValues)
+                }
+                {...commonProps}
+                sx={{ width: '25%' }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">€</InputAdornment>
+                  ),
+                }}
+              />
             </Box>
           </Box>
-        </AccordionDetails>
-      </Accordion>
-    </Card>
+          <Typography variant="subtitle2" gutterBottom mt={2}>
+            Energy consumption metrics
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+            <Box>
+              <Typography variant="body2" gutterBottom>
+                Total Electricity Demand
+              </Typography>
+              <NumberInput
+                value={electricityDemand !== null ? electricityDemand : ''}
+                onValueChange={(values) =>
+                  setElectricityDemand(values.floatValue ?? null)
+                }
+                onBlur={(e) =>
+                  handleBlur('electricityDemand')(
+                    e as unknown as NumberFormatValues
+                  )
+                }
+                {...commonProps}
+                sx={{ width: '25%' }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">MWh</InputAdornment>
+                  ),
+                }}
+              />
+            </Box>
+            <Box>
+              <Typography variant="body2" gutterBottom>
+                Total Heating Demand
+              </Typography>
+              <NumberInput
+                value={heatingDemand !== null ? heatingDemand : ''}
+                onValueChange={(values) =>
+                  setHeatingDemand(values.floatValue ?? null)
+                }
+                onBlur={(e) =>
+                  handleBlur('heatingDemand')(
+                    e as unknown as NumberFormatValues
+                  )
+                }
+                {...commonProps}
+                sx={{ width: '25%' }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">GWh</InputAdornment>
+                  ),
+                }}
+              />
+            </Box>
+          </Box>
+        </Box>
+      </AccordionDetails>
+    </Accordion>
   );
 };
 
-export default EssentialDataPanel;
+export default EssentialData;
