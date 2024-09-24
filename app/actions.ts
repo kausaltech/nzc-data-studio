2a6fa1e5f4799b2da14bf4e194c68ec1e81b11e2
Joe Smallwood <joe.smallwood@kausal.tech>
2024-09-24 11:46:19 +0300

    Use backend sign out to log out of the admin UI

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,6 @@
-"use server";
+'use server';
 
-import {
-  auth,
-  signOut,
-} from '@/config/auth';
+import { auth, signOut } from '@/config/auth';
 import { authIssuer } from '@/constants/environment';
 
 export async function handleBackendSignOut(currentUri: string) {
@@ -11,6 +8,9 @@ export async function handleBackendSignOut(currentUri: string) {
   const idToken = session?.idToken;
   if (!idToken) return null;
   await signOut({ redirect: false });
-  const redirectUri = `${authIssuer}/o/logout/?post_logout_redirect_uri=${encodeURI(currentUri)}&id_token_hint=${idToken}`;
+  const redirectUri = `${authIssuer}/o/logout/?post_logout_redirect_uri=${encodeURI(
+    currentUri
+  )}&id_token_hint=${idToken}`;
+
   return redirectUri;
 }
