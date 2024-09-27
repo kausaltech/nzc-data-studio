f83194d6c92134f5b222ca15d838eab8d55b9f1f
Joe Smallwood <joe.smallwood@kausal.tech>
2024-09-27 12:16:18 +0300

    Add initial root error page

---
+++ app/error.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useEffect } from 'react';
+import * as Sentry from '@sentry/nextjs';
+import {
+  Card,
+  CardContent,
+  Container,
+  Link,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { ExclamationCircle } from 'react-bootstrap-icons';
+import { useSession } from 'next-auth/react';
+
+type Props = {
+  error: Error & { digest?: string };
+};
+
+export default function Error({ error }: Props) {
+  const session = useSession();
+
+  useEffect(() => {
+    Sentry.captureException(error, session);
+  }, [error, session]);
+
+  return (
+    <Container>
+      <Card>
+        <CardContent>
+          <Stack direction="row" spacing={2} alignItems="center" mb={2}>
+            <ExclamationCircle size={32} />
+            <Typography component="h1" variant="h2">
+              Something went wrong
+            </Typography>
+          </Stack>
+          <Typography variant="subtitle1" color="text.secondary">
+            We're sorry, but something went wrong on our end. Please try
+            refreshing the page or signing out and in again. If that doesn't
+            help, please get in touch with us at{' '}
+            <Link href="mailto:support@kausal.tech">support@kausal.tech</Link>{' '}
+            for assistance.
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+}
