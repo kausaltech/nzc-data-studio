29467ff0f6700332452ddc50a183270154bc0132
Lilia Sviridova <svirivoda@gmail.com>
2024-11-11 16:31:45 +0200

    Refactor additional page

--- app/additional-data/page.tsx
+++ app/additional-data/page.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { Container, Typography, Button, Stack, Box, Fade } from '@mui/material';
+import {
+  Container,
+  Typography,
+  Stack,
+  Box,
+  Fade,
+  Tooltip,
+} from '@mui/material';
 import { QuestionCircle } from 'react-bootstrap-icons';
 import { useFrameworkInstanceStore } from '@/store/selected-framework-instance';
 import Tip from '@/components/Tip';
@@ -9,27 +16,33 @@ import { AdditionalDatasheetEditor } from '@/components/AdditionalDatasheetEdito
 export default function AdditionalDataPage() {
   const baselineYear = useFrameworkInstanceStore((state) => state.baselineYear);
 
-  const handleButtonClick = () => {
-    console.log('The Modal!');
-  };
-
   return (
     <Fade in>
       <Container>
         <Stack direction="row" alignItems="baseline" spacing={1}>
-          <Typography variant="h3">Additional Historical Data</Typography>
+          <Typography component="h1" variant="h3">
+            Additional Historical Data
+          </Typography>
           <Typography color="textSecondary">(Optional)</Typography>
         </Stack>
         <Box display="flex" alignItems="flex-start" mt={1}>
           <Typography>Baseline {baselineYear}</Typography>
-          <Button
-            size="small"
-            variant="text"
-            color="inherit"
-            endIcon={<QuestionCircle size={14} />}
-            onClick={handleButtonClick}
-            sx={{ minWidth: 'auto', padding: 0 }}
-          />
+          <Tooltip
+            placement="top"
+            arrow
+            title={
+              <Typography variant="body2">
+                The baseline year represents the starting point for measuring
+                your city's emissions. This year provides a reference for
+                tracking progress over time and should ideally be a recent year
+                for which accurate data is available.
+              </Typography>
+            }
+          >
+            <div style={{ marginLeft: 4, cursor: 'pointer' }}>
+              <QuestionCircle size={16} />
+            </div>
+          </Tooltip>
         </Box>
         <Tip
           title="Estimate emissions for past years and track your progress"
